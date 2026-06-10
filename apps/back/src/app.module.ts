6029193431424c664148647820beb1b3c86e7f94
matheuslemesam <matheuslemesam@gmail.com>
2026-06-10 12:19:53 -0300

    fix: remove código morto (UsersModule, AppController, AppService)

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -3,8 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard } from '@nestjs/throttler';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { StorageModule } from './storage/storage.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -30,7 +28,7 @@ import { NotificacaoModule } from './notificacao/notificacao.module';
     ProcessoSeletivoModule,
     NotificacaoModule,
   ],
-  controllers: [AppController, HealthController],
-  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
+  controllers: [HealthController],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
