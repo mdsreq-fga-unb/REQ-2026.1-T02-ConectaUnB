d47cd10b9246e7f37a21e4c891e245cb2c710176
Pedro Americo <dev.americo@gmail.com>
2026-06-15 16:55:48 -0300

    fix: correção de erros na página de perfil

--- apps/front/app/login/page.tsx
+++ apps/front/app/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { api } from "../../guards/api";
