aad42e05a17efe4a33da46a0dbc997b37826801d
Pedro Americo <dev.americo@gmail.com>
2026-06-28 18:16:50 -0300

    feat: Adição de botão voltar nas páginas de login e cadastro
    Co-authored-by: Gabriel Diniz <12gabrieldiniz@gmail.com>

--- apps/front/app/auth/login/page.tsx
+++ apps/front/app/auth/login/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { api } from "@/guards/api";
 import { useAuth } from "@/hooks/useAuth";
 import AuthBanner from "@/components/auth/AuthBanner";
+import { BackButton } from "@/components/Voltar";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -67,13 +68,18 @@ try {
     }
   };
 
-  return (
+return (
     <main className="min-h-screen grid grid-cols-1 md:grid-cols-2 text-[#1D1D1D]">
         
         <AuthBanner/>
 
-      {/* Seção do Formulário de Login */}
-      <section className="flex flex-col items-center justify-center bg-white p-8 sm:p-12">
+      <section className="relative flex flex-col items-center justify-center bg-white p-8 sm:p-12">
+        
+        <div className="absolute top-6 left-6 sm:top-8 sm:left-8">
+          <BackButton />
+        </div>
+
+        {/* O restante continua igualzinho, perfeitamente centralizado */}
         <div className="w-full max-w-md space-y-8">
           
           <div className="text-center">
